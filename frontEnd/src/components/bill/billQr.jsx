2be7a5af76ed59231d266854efe5f825e7075542
Makardhwaj <mhs93679@gmail.com>
2024-08-19 11:32:54 +0530

    fixed the qr download

--- frontEnd/src/components/bill/billQr.jsx
+++ frontEnd/src/components/bill/billQr.jsx
@@ -1,24 +1,35 @@
 import QRCode from 'qrcode.react'; 
+import html2canvas from 'html2canvas';
+import { useRef } from 'react';
 
 export default function billQr({pdf,showqr,setshowqr}){
+    const qrCodeRef = useRef();
+    const downloadQRCode = () => {
+        html2canvas(qrCodeRef.current).then((canvas) => {
+            const imgData = canvas.toDataURL('image/png');
+            const link = document.createElement('a');
+            link.href = imgData;
+            link.download = `qrcode-${pdf.serialNO}.png`; // Set the file name
+            link.click();
+        });
+    };
     return(
         <>
            <div className="cardimage">
-                        <div id={`qr-container-${pdf.serialNO}`} className= "qr-container Qrhidden"
+                        <div id={`qr-container-${pdf.serialNO}`} className= "qr-container Qrhidden"  
                         >
+                            <div ref={qrCodeRef}>
                             <QRCode
+                            
                                 id={`qr-code-${pdf.serialNO}`}
                                 value={`http://localhost:8080/pdf/${pdf.serialNO}`}
                                 className="qr-code"
-                            />
-                            <a
-                                id={`download-link-${pdf.serialNO}`}
-                                className="download-btn"
-                                href={`/api/qrcode/${pdf.serialNO}`} // Endpoint to generate/download QR code
-                                download={`qrcode-${pdf.name}.png`}
-                            >
-                                <button>Download QR</button>
-                            </a>
+                                size={220} // Adjust size as needed
+                                level="H"  // Error correction level (L, M, Q, H)
+                                includeMargin={true} // Include margin around the QR code
+                            /></div>
+                           
+                                <button onClick={downloadQRCode}>Download QR</button>     
                             <hr />
                         </div>
             </div>
