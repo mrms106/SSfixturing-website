ee38d6de96547131cc4ee07fc12d3e0ab5cf4347
Makardhwaj <mhs93679@gmail.com>
2024-08-19 22:35:05 +0530

    specify the user

--- backend/middelware/cheackUser.js
+++ backend/middelware/cheackUser.js
@@ -1,6 +1,6 @@
 module.exports.cheackUser=(req,res,next)=>{
      const currUser=req.user
-     if(currUser.username==="ShubhamShinde" && currUser.email==="ssfixturing1@gmail.com"){
+     if(currUser.username===process.env.siteuser && currUser.email===process.env.userMail){
         next()
      }else{
         res.redirect('/')
