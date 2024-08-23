72b99540a02ad07695a376dda2179cc073ba4e5e
Makardhwaj <mhs93679@gmail.com>
2024-08-23 16:08:35 +0530

    added delete bill alerts

--- frontEnd/src/components/bill/billButtons.jsx
+++ frontEnd/src/components/bill/billButtons.jsx
@@ -1,18 +1,38 @@
 
+import Swal from 'sweetalert2';
 import DeleteButton from './deleteButton'
 export default function billButtons({pdfs,pdf,setPdfs,toggleQRCode,showqr,setshowqr}){
 
    
 
     const handleDelete = async (serialNO) => {
+        Swal.fire({
+            title: 'Deleting...',
+            text: 'Please wait for few seconds...',
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            didOpen: () => {
+                Swal.showLoading();
+            }
+        });
         try {
             const response = await fetch(`https://ssfixturing.com/api/pdf/${serialNO}`, {
                 method: 'DELETE',
                 credentials:'include'
             });
             if (!response.ok) throw new Error('Network response was not ok');
+            Swal.close()
+            Swal.fire({
+                title: 'Bill Deleted',
+                text:  'Bill Deleted Succefully',
+                icon: 'success',
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                confirmButtonText: 'OK'
+            })
             setPdfs(pdfs.filter(pdf => pdf.serialNO !== serialNO));
         } catch (error) {
+            Swal.close()
             console.error('Error deleting PDF:', error);
         }
     };
