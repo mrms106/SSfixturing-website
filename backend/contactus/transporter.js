c1929824fe291be997870bbb47793daaf303f4be
Makardhwaj <mhs93679@gmail.com>
2024-08-27 14:27:10 +0530

    change the mail address

--- backend/contactus/transporter.js
+++ backend/contactus/transporter.js
@@ -4,12 +4,12 @@ const nodemailer=require("nodemailer");
 
 // mail send
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    host: 'mail.ssfixturing.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.mail, 
-        pass: process.env.pass 
+        pass: process.env.pass
     },
     // logger: true,
     // debug: true
