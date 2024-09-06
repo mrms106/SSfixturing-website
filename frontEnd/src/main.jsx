f6c69d96306f816e3ce84b31770e342ca9f74abf
Makardhwaj <mhs93679@gmail.com>
2024-09-06 13:01:39 +0530

    when user add shortcut it will work like mobile app

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -7,3 +7,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    const swUrl = `${import.meta.env.VITE_PUBLIC_URL || ''}/serviceWorker.js`;
+    if (swUrl) {
+      navigator.serviceWorker.register(swUrl)
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    } else {
+      console.warn('Service Worker URL is not defined');
+    }
+  });
+}
