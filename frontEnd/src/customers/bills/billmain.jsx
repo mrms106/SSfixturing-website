25c317b5ca2b9be0ced5638d7bf59e08a2832a71
Makardhwaj <mhs93679@gmail.com>
2025-01-27 17:50:08 +0530

    created the seperate file for bill inputs

--- frontEnd/src/customers/bills/billmain.jsx
+++ frontEnd/src/customers/bills/billmain.jsx
@@ -1,9 +1,7 @@
 import React, { useState,useEffect } from 'react';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import BillInput from './billinput';
 
 export default function CreateBill({ customer, setcreatebill,fetchBill }) {
-  const [IsOutside, setIsOutside] = useState(false);
   const [formData, setFormData] = useState({
     cname: customer.name,
     caddress: customer.address,
@@ -25,28 +23,7 @@ export default function CreateBill({ customer, setcreatebill,fetchBill }) {
       },
     ],
   });
-// Sync IsOutside state with formData
-useEffect(() => {
-  setFormData((prev) => ({
-    ...prev,
-    isOutside: IsOutside,
-  }));
-}, [IsOutside]);
-  // Handle changes in form fields
-  const handleChange = (e, index = null) => {
-    const { name, value } = e.target;
-
-    if (index !== null) {
-      // Update item array
-      const updatedItems = [...formData.item];
-      updatedItems[index][name] = value;
-      setFormData({ ...formData, item: updatedItems });
-    } else {
-      // Update top-level fields
-      setFormData({ ...formData, [name]: value });
-    }
-  };
-console.log(IsOutside)
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -72,19 +49,7 @@ console.log(IsOutside)
     }
   };
 
-  const addItem = () => {
-    setFormData({
-      ...formData,
-      item: [
-        ...formData.item,
-        { description: '',  unitRate: '', quantity: '',  discount: '' },
-      ],
-    });
-  };
-  const removeItem = (index) => {
-    const updatedItems = formData.item.filter((_, i) => i !== index);
-    setFormData({ ...formData, item: updatedItems });
-  };
+  
   return (
     <div className="addcustomer-main">
       <div className="addcustomer-close-btn">
@@ -92,130 +57,7 @@ console.log(IsOutside)
         <h3>Create Bill</h3>
         
       </div>
-      <form onSubmit={handleSubmit} className='create-bill-form'>
-        <div className='create-bill-form-div1'>
-         <div>
-         <label>Invoice No</label>
-          <input
-            type="text"
-            name="invoiceNo"
-            placeholder="Enter invoice no."
-            required
-            value={formData.invoiceNo}
-            onChange={handleChange}
-          />
-         </div>
-
-         <div>
-         <label>PO No</label>
-          <input
-            type="text"
-            name="PoNo"
-            placeholder="Enter PO no."
-            required
-            value={formData.PoNo}
-            onChange={handleChange}
-          />
-         </div>
-
-         <div>
-         <label>Invoice Date</label>
-          <input
-            type="date"
-            name="invoicedate"
-            required
-            value={formData.invoicedate}
-            onChange={handleChange}
-          />
-         </div>
-
-          <div>
-          <label>PO Date</label>
-          <input
-            type="date"
-            name="Podate"
-            required
-            value={formData.Podate}
-            onChange={handleChange}
-          />
-          </div>
-         
-        </div>
-        <div style={{marginRight:'90%'}}>
-        <FormControlLabel
-            control={<Switch checked={IsOutside} onChange={() => setIsOutside(!IsOutside)} />}
-            label="IsOutside"
-          />
-            
-          </div>
-        {formData.item.map((item, index) => (
-          <>
-          <h3>Product {index+1}</h3>
-          <div key={index} className='create-bill-form-div2'>
-            {/* <h3>item {index+1}</h3> */}
-            <div>
-            <label>Description</label>
-            <input
-              type="text"
-              name="description"
-              placeholder="Enter description"
-              required
-              value={item.description}
-              onChange={(e) => handleChange(e, index)}
-            />
-            </div>
-
-            <div>
-            <label>Unit Rate</label>
-            <input
-              type="text"
-              name="unitRate"
-              placeholder="Enter unit rate"
-              required
-              value={item.unitRate}
-              onChange={(e) => handleChange(e, index)}
-            />
-            </div>
-
-           <div>
-           <label>Quantity</label>
-            <input
-              type="number"
-              name="quantity"
-              placeholder="Enter quantity"
-              required
-              value={item.quantity}
-              onChange={(e) => handleChange(e, index)}
-            />
-           </div>
-
-           
-           <div>
-           <label>Discount (%)</label>
-            <input
-              type="number"
-              name="discount"
-              placeholder="Enter discount"
-              required
-              value={item.discount}
-              onChange={(e) => handleChange(e, index)}
-            />
-           </div>
-           <button type="button" onClick={() => removeItem(index)}>
-              Remove Item
-            </button>
-          </div></>
-        ))}
-        
-
-        <div className='create-bill-form-buttons'>
-        <button type="button" onClick={addItem}>
-          Add Item
-        </button>
-        <button type="submit">Create Bill</button>
-        </div>
-        
-      </form>
+    <BillInput handleSubmit={handleSubmit} customer={customer} formData={formData} setFormData={setFormData}/>
     </div>
   );
 }
