a0df951a1dd54554620e6fd712c74b9443eed4e2
Makardhwaj <mhs93679@gmail.com>
2025-01-28 12:07:37 +0530

    added islogged in to the routes

--- backend/router/bill.js
+++ backend/router/bill.js
@@ -2,11 +2,12 @@ const billcontroller=require("../controllers/bill")
 const express=require("express")
 const router=express.Router()
 const {cheackUser}=require("../middelware/cheackUser")
+const { isLoggedIn } = require("../middelware/isloggedin");
 
-router.post('/createbill',cheackUser, billcontroller.createBill);       
-router.put('/bill/:billId',cheackUser, billcontroller.updateBill);    
-router.delete('/bill/:billId',cheackUser, billcontroller.deleteBill); 
+router.post('/createbill',cheackUser,isLoggedIn, billcontroller.createBill);       
+router.put('/bill/:billId',cheackUser,isLoggedIn, billcontroller.updateBill);    
+router.delete('/bill/:billId',cheackUser,isLoggedIn, billcontroller.deleteBill); 
 router.get('/bill/:billId', billcontroller.getBillByInvoiceNo);
-router.post('/bills',cheackUser,billcontroller.getBillsBySerialNos)
-router.patch('/bills/:billId',cheackUser,billcontroller.updatecreditedamount)
+router.post('/bills',cheackUser,isLoggedIn,billcontroller.getBillsBySerialNos)
+router.patch('/bills/:billId',cheackUser,isLoggedIn,billcontroller.updatecreditedamount)
 module.exports = router;
--- backend/router/customer.js
+++ backend/router/customer.js
@@ -2,13 +2,14 @@ const express=require('express')
 const router=express.Router()
 const customerController=require('../controllers/customer')
 const {cheackUser}=require("../middelware/cheackUser")
+const { isLoggedIn } = require("../middelware/isloggedin");
 
-router.get('/allcustomers',cheackUser,customerController.showCustomers)
-router.get('/customer/:serialNo',cheackUser,customerController.showonecustomer)
-router.post('/addcustomer',cheackUser,customerController.addcustomer)
-router.delete('/deletecustomer/:serialNo',cheackUser,customerController.deletecustomer)
-router.put('/customer/:serialNo', cheackUser,customerController.updateCustomer);
-router.patch('/customer/:serialNo',cheackUser,customerController.updateAmounts)
+router.get('/allcustomers',cheackUser,isLoggedIn,customerController.showCustomers)
+router.get('/customer/:serialNo',cheackUser,isLoggedIn,customerController.showonecustomer)
+router.post('/addcustomer',cheackUser,isLoggedIn,customerController.addcustomer)
+router.delete('/deletecustomer/:serialNo',isLoggedIn,cheackUser,customerController.deletecustomer)
+router.put('/customer/:serialNo',isLoggedIn, cheackUser,customerController.updateCustomer);
+router.patch('/customer/:serialNo',isLoggedIn,cheackUser,customerController.updateAmounts)
 
 
 
